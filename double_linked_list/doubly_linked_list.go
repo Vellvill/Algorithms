8f0e0fb8dac6cdaf42538e6cba70fba6d29ad0ea
Gerasimov Timofey <gerasimoff.timofeii@yandex.ru>
2022-04-02 03:37:04 +0300

    ll2

--- double_linked_list/doubly_linked_list.go
+++ double_linked_list/doubly_linked_list.go
@@ -1,1 +1,39 @@
 package double_linked_list
+
+type DLL struct {
+	nextNode *DLL
+	prevNode *DLL
+	data     int
+}
+
+func NewDLL(v int) *DLL {
+	return &DLL{nil, nil, v}
+}
+
+func (n *DLL) AddNode(v int) {
+	for i := n; i != nil; i = i.nextNode {
+		if i.nextNode == nil {
+			new := NewDLL(v)
+			n.nextNode = new
+			break
+		}
+	}
+}
+
+func (n *DLL) FindMiddle() *DLL {
+	for i, j := n, n.FindHead(); i != j; i, j = i.nextNode, j.prevNode {
+		if i == j {
+			return i
+		}
+	}
+	return nil
+}
+
+func (n *DLL) FindHead() *DLL {
+	for i := n; i != nil; i = i.nextNode {
+		if i.nextNode == nil {
+			return i
+		}
+	}
+	return nil
+}
