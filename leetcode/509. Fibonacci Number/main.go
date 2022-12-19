39639bde96069c4558fcc017f047a20842d82094
Vellvill <gerasimoff.timofeii@yandex.ru>
2022-12-19 15:00:01 +0400

    Fibonacci

---
+++ leetcode/509. Fibonacci Number/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var a int
+    _, _ = fmt.Scan(&a)
+    fib(a)
+}
+
+//rec
+func fibRec(n int) int {
+    if n <= 1 {
+        return n
+    }
+    return fibRec(n-1) + fibRec(n-2)
+}
+
+//non rec
+func fibNoRec(n int) int {
+    var f = make([]int, n+1, n+2)
+    if n < 2 {
+        f = f[0:2]
+    }
+    f[0], f[1] = 0, 1
+    for i := 2; i <= n; i++ {
+        f[i] = f[i-1] + f[i-2]
+    }
+    return f[n]
+}
--- main.go
+++ main.go
@@ -1,18 +1,3 @@
 package main
 
-import (
-    "algorithms/array"
-)
-
-var custom struct {
-    error
-}
-
-func main() {
-    a := array.MakeArray()
-    a.Append(1, 2, 3, 4, 5, 6, 7)
-}
-
-func test() {}
-
-func test2() {}
+func main() {}
