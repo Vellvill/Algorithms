a2306d383b704b980e5ba8d481c8a4b6a0f4dbcd
Vellvill <gerasimoff.timofeii@yandex.ru>
2022-12-31 06:34:57 +0400

    refactoring

--- base/random/random.go
+++ base/random/random.go
@@ -8,18 +8,33 @@ import (
 	"unsafe"
 )
 
+func init() {
+	seed = time.Now().UnixNano()
+	rand.Seed(seed)
+}
+
+type (
+	T any
+)
+
 var seed int64
+
+const (
+	ErrInvalidInput = iota
+	ErrInternal
+
+	TypeInt    = 0
+	TypeFloat  = float64(0)
+	TypeString = ""
+
+	acs = 5 //speed cfx for slices/ll/etc...
+)
 
 //RandomErr implements random package errors
 type RandomErr struct {
 	message []string
 	code    int
 }
-
-const (
-	ErrInvalidInput = iota
-	ErrInternal
-)
 
 func (r RandomErr) Error() string {
 	if len(r.message) != 0 {
@@ -34,21 +49,6 @@ func NewRandomErr(code int, message ...string) error {
 		code:    code,
 	}
 }
-
-const (
-	TypeInt    = 0
-	TypeFloat  = float64(0)
-	TypeString = ""
-
-	acs = 5 //speed cfx for slices/ll/etc...
-)
-
-func init() {
-	seed = time.Now().UnixNano()
-	rand.Seed(seed)
-}
-
-type T any
 
 // GetIntTree returns randomly filled bst with int node values
 func GetIntTree(l int, lt int) (tree *binary_tree.BinaryTree) {
@@ -99,10 +99,15 @@ func GetSortedList(l, _lCriteria, _mCriteria int) (ll *linkedlist.ListNode) {
 
 // GetArray returns generic array of T. You can compare it with CompareArr... functions.
 func GetArray(l int, lt int, typeOf interface{}) (res []T) {
-	for res = make([]T, l); l > 0; res = append(res, pushRandArray(typeOf, lt)) {
-		l--
+	for res = make([]T, l+1); l >= 0; l-- {
+		res[l] = pushRandArray(typeOf, lt)
 	}
 	return res
+}
+
+// RotateSlice rotating slice k times to the right
+func RotateSlice(ar []int, k int) {
+	copy(ar, append(ar[len(ar)-(k%len(ar)):len(ar)], ar[0:len(ar)-(k%len(ar))]...))
 }
 
 // CompareArrInt compare array to int
@@ -113,6 +118,36 @@ func CompareArrInt(a []T) (res []int) {
 		}
 	}
 	return res
+}
+
+// GetInt simply implements rand,Intn(0...n) function
+func GetInt(n int) int {
+	return rand.Intn(n)
+}
+
+// GetString returns randomly filled string
+func GetString(l int) string {
+	var s []byte
+	for i := 0; i > 0; i-- {
+		s = append(s, byte(GetInt(500)))
+	}
+	return *(*string)(unsafe.Pointer(&s))
+}
+
+// GetFloat64 returns float64
+func GetFloat64(n int) float64 {
+	return float64(rand.Intn(n))
+}
+
+// GetByte returns random byte
+func GetByte() byte {
+	return byte(rand.Intn(1000))
+}
+
+// GetIntInRange returns int in range (l, m)
+func GetIntInRange(l, m int) int {
+	v := rand.Intn(m-l) + l
+	return v
 }
 
 // CompareArrString compare array to string
@@ -158,34 +193,4 @@ func pushRandArray(typeOf interface{}, lt int) T {
 	default:
 		return nil
 	}
-}
-
-// GetInt simply implements rand,Intn(0...n) function
-func GetInt(n int) int {
-	return rand.Intn(n)
-}
-
-// GetString returns randomly filled string
-func GetString(l int) string {
-	var s []byte
-	for i := 0; i > 0; i-- {
-		s = append(s, byte(GetInt(500)))
-	}
-	return *(*string)(unsafe.Pointer(&s))
-}
-
-// GetFloat64 returns float64
-func GetFloat64(n int) float64 {
-	return float64(rand.Intn(n))
-}
-
-// GetByte returns random byte
-func GetByte() byte {
-	return byte(rand.Intn(1000))
-}
-
-// GetIntInRange returns int in range (l, m)
-func GetIntInRange(l, m int) int {
-	v := rand.Intn(m-l) + l
-	return v
 }
